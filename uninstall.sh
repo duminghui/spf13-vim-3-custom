e8b1d02be530f0ad6dde24c44b6b6cccb0cb0ab4
duminghui <duminghui@139.com>
2016-05-20 00:51:43 +0800

    add .gitignore bootstrap.sh uninstall.sh

---
+++ uninstall.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env sh
+
+app_dir="$HOME/.spf13-vim-3-custom"
+
+warn() {
+    echo "$1" >&2
+}
+
+die() {
+    warn "$1"
+    exit 1
+}
+
+rm $HOME/.vimrc.before.local
+rm $HOME/.vimrc.bundles.local
+rm $HOME/.vimrc.local
+
+#rm -rf $app_dir
